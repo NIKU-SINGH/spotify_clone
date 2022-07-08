2055ef3d9e317b9af68b9a3fe49e7b71401262e7
NIKU-SINGH <soapmactavishmw4@gmail.com>
2022-07-08 15:45:21 +0530

    🐛

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -56,7 +56,7 @@ export default NextAuth({
         "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
     }),
   ],
-  secret: process.env.JWT_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
   callbacks: {
     async jwt({ token, user, account }) {
       // Initial sign in
